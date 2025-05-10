43e591a087f3d641739f34443f3b4c095ca7fdd9
riceboat <gregorwatt1@gmail.com>
2025-05-10 18:19:48 +0100

    Added ask price functionality and request builder

--- src/dailyTrader/APIManager.java
+++ src/dailyTrader/APIManager.java
@@ -1,5 +1,71 @@
 package dailyTrader;
 
-public class APIManager {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.json.*;
+
+public class APIManager {
+	String public_key;
+	String private_key;
+
+	public APIManager(String public_key, String private_key) {
+		this.public_key = public_key;
+		this.private_key = private_key;
+	}
+
+	public float getAskPrice(String symbol) {
+		float price = 0.0f;
+		HashMap<String, String> args = new HashMap<String, String>();
+		args.put("symbols", symbol);
+		String request = requestBuilder("stocks/quotes/latest", args);
+		String response = APIRequest(request);
+		JSONObject obj = new JSONObject(response);
+		price = (float) obj.getJSONObject("quotes").getJSONObject(symbol).get("ap");
+		return price;
+	}
+
+//	public abstract float getBidPrice();
+//
+//	public abstract float buyStock();
+
+	public String requestBuilder(String path, Map<String, String> args) {
+		String prefix = "https://data.alpaca.markets/v2/";
+		String result = prefix + path;
+		int s = args.size();
+		if (s != 0) {
+			result += "?";
+			s--;
+			for (Map.Entry<String, String> entry: args.entrySet()) {
+				result += entry.getKey() + "=" + entry.getValue();
+			}
+			if (s != 0) {
+				result +="&";
+			}
+		}
+		return result;
+	}
+
+	public String APIRequest(String requestString) {
+		//TODO add rate limit headers
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(requestString))
+				.header("accept", "application/json")
+			    .header("APCA-API-KEY-ID", public_key)
+			    .header("APCA-API-SECRET-KEY", private_key)
+			    .method("GET", HttpRequest.BodyPublishers.noBody())
+			    .build();
+		HttpResponse<String> response;
+		try {
+			response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+			return response.body();
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
--- src/dailyTrader/MainClass.java
+++ src/dailyTrader/MainClass.java
@@ -1,8 +1,12 @@
 package dailyTrader;
 
+import AlgoTrader.APIManager;
+
 public class MainClass {
 	public static void main(String args[]) {
 		String public_key = "PKUM7ULODBBRTDICG49P";
 		String private_key = "0JQPuCrrdhQOcTrTqnB8p7sSYVp2mtxYXqPSNUez";
+		APIManager apiManager = new APIManager(public_key, private_key);
+		apiManager.getAskPrice("AAPL");
 	}
 }
