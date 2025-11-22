6ecb3fe89f78d81a1057d7d8c3b22294636b70f8
riceboat <gregorwatt1@gmail.com>
2025-11-22 01:46:08 +0000

    Forgot to add new file

---
+++ src/strategies/SingleStockSMACrossover.java
@@ -0,0 +1,41 @@
+package strategies;
+
+import java.util.ArrayList;
+import backTesting.TradingAction;
+import dailyTrader.Bars;
+import dailyTrader.Market;
+import dailyTrader.Portfolio;
+import dailyTrader.Side;
+
+public class SingleStockSMACrossover extends Strategy {
+
+	public SingleStockSMACrossover(int longMA, int shortMA) {
+		setParameterValue("longMA", longMA);
+		setParameterValue("shortMA", shortMA);
+	}
+
+	@Override
+	public ArrayList<TradingAction> decide(Market market, Portfolio portfolio, ArrayList<TradingAction> possibleActions,
+			int day) {
+		ArrayList<TradingAction> chosenActions = new ArrayList<TradingAction>();
+		int longMA = (int) getParameterValue("longMA");
+		int shortMA = (int) getParameterValue("shortMA");
+		for (TradingAction action : possibleActions) {
+			Bars actionBars = market.getSymbolBars(action.getSymbol());
+			double longMAValue = actionBars.getSMALastNDays(longMA);
+			double shortMAValue = actionBars.getSMALastNDays(shortMA);
+			if (action.getSide() == Side.LONG && shortMAValue > longMAValue) {
+				chosenActions.add(action);
+			}
+			if (action.getSide() == Side.SELL && shortMAValue < longMAValue) {
+				chosenActions.add(action);
+			}
+			if (action.getSide() == Side.SHORT && shortMAValue < longMAValue) {
+				chosenActions.add(action);
+			}
+		}
+
+		return chosenActions;
+	}
+
+}
